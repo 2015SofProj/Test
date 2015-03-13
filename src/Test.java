bd34bdbed9985a9a15de0168541da80d73d7fa9a
jETA-Kor <jeta@jetalog.net>
2015-03-13 09:55:16 +0900

    Add a loop.
    Prints ten times.

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
-
+		for(int i = 0; i < 10; i++)
+			System.out.println("Hello world");
 	}
 
 }
